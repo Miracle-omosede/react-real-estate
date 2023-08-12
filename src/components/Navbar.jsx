6974bc8b910e2e1785705806102db2edc5816cc5
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-08-12 22:44:26 +0100

    worked on the  Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const Navbar = () => {
+  return (
+    <nav className='flex items-center justify-between py-10'>
+
+        {/* --logo wrappper-- */}
+        <div>
+            <h1 className='font-semibold text-[22px] capitalize font-Bricolage'>lyfewide</h1>
+        </div>
+
+        {/* --nav wrapper--  */}
+        <div className='flex items-center justify-between gap-16'>
+            {/* ----buttons----- */}
+            <div>
+                <ul className='font-Bricolage flex flex-row gap-4 font-[500] text-[17px]'>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Appartments</a></li>
+                    <li><a href="#">What's New</a></li>
+                </ul>
+            </div>
+            {/* ----icons------ */}
+            <div className='flex gap-10 items-center justify-center text-[18px]'>
+                <span><i class="ri-search-line"></i></span>
+                <span><i class="ri-menu-3-fill"></i></span>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
