301c90d2de85b7bd67ea05929090e4e2ed4e0455
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-08-13 20:58:01 +0100

    About page setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import About from './pages/About'
 
 const App = () => {
   return (
     <div className='w-full max-w-[1100px] mx-auto'>
         <Navbar/>
         <Hero/>
+        <About/>
     </div>
   )
 }
