4a83ad408cb47bb19c1c88700e8b689d6fc689ce
Miracle-omosede <miracleigbinoghene@gmail.com>
2023-09-02 10:44:56 +0100

    testing

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -10,10 +10,6 @@ const About = () => {
         <div className='relative w-full h-full'>
           <img src={ villa } alt="" className='absolute top-0 right-0 w-full h-[700px] object-cover' />
           <div className='image-overflow'/>
-          {/* <div className='w-full h-[700px] text-white absolute flex items-center justify-bottom bottom-0 left-0'>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, beatae magnam. Natus ea, ipsam aliquid aut in quas. Ratione nisi quas a omnis hic consequuntur harum saepe excepturi sint eaque eligendi beatae id laboriosam perspiciatis
-          </div> */}
-
         </div>
         <div>
         <div className='absolute bottom-24 left-0 text-white w-full px-5 md:px-56 font-poppins' > 
